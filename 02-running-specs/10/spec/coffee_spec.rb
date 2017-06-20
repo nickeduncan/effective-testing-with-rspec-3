64a7046a444e13c0e31b7e6e37100db0c48108e9
Nick Duncan <nickeduncan@gmail.com>
2017-06-20 12:53:09 -0400

    marked incomplete work

---
+++ 02-running-specs/10/spec/coffee_spec.rb
@@ -0,0 +1,43 @@
+class Coffee
+	def ingredients
+		@ingredients ||= []
+	end
+
+	def add(ingredient)
+		ingredients << ingredient
+	end
+
+	def price
+		1.00 + ingredients.size * 0.25
+	end
+end
+
+RSpec.configure do |config|
+	# config.example_status_persistence_file_path = 'spec/examples.txt'
+end
+
+RSpec.describe 'A cup of coffee' do
+	let(:coffee) { Coffee.new }
+
+	it 'costs $1' do
+		expect(coffee.price).to eq(1.00)
+	end
+
+	context 'with milk', focus: true do
+		before { coffee.add :milk }
+
+		it 'costs $1.25' do
+			expect(coffee.price).to eq(1.25)
+		end
+
+		it 'is light in color' do
+			pending 'Color not implemented yet'
+			expect(coffe.color).to be(:light)
+		end
+
+		it 'is cooler than 200 degress Fahrenheit' do
+			pending 'Temperature not implemented yet'
+			expect(coffee.temperature).to be < 200.0
+		end
+	end
+end
