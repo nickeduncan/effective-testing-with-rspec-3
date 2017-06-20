f6bc03bb30e9ce67c26b86b73018943769f15a02
Nick Duncan <nickeduncan@gmail.com>
2017-06-20 11:49:45 -0400

    formatting rspec for failures

---
+++ 02-running-specs/03/spec/slow_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe 'the sleep() method' do
+   it('can sleep for 0.1') { sleep 0.1 }
+   it('can sleep for 0.2') { sleep 0.2 }
+   it('can sleep for 0.3') { sleep 0.3 }
+   it('can sleep for 0.4') { sleep 0.4 }
+   it('can sleep for 0.5') { sleep 0.5 }  
+end
