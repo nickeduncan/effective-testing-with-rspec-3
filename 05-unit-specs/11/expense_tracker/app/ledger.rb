2c21e205711922ecf127420a7766c30a937439d5
Nick Duncan <nickeduncan@gmail.com>
2017-06-26 11:29:30 -0400

    finished integrations specs and practice app

--- 05-unit-specs/11/expense_tracker/app/ledger.rb
+++ 05-unit-specs/11/expense_tracker/app/ledger.rb
@@ -2,7 +2,11 @@ module ExpenseTracker
   RecordResult = Struct.new(:success?, :expense_id, :error_message)
 
   class Ledger
-    def record
+    def record(expense)
+
+    end
+
+    def expenses_on(date)
 
     end
   end
---
+++ 05-unit-specs/11/expense_tracker/config.ru
@@ -0,0 +1,2 @@
+require_relative 'app/./../04-acceptance-specs/05/expense_tracker/app/api'
+run ExpenseTracker::API.new
